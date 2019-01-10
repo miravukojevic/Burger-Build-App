f3107154ef1868c70feade10c07310ea6aa3e8e8
Mirjana Vukojevic <mirjana.vukojevic@quantox.com>
2019-01-10 13:56:17 +0100

    big change

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            I love you
           </a>
         </header>
       </div>
